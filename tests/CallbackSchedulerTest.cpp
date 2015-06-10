ae82f4336dae06548dba9ca08edbf96c115be3c4
Indrek Luuk <indrek.luuk@gmail.com>
2015-06-10 21:52:48 +0300

    test CallbackScheduler

---
+++ tests/CallbackSchedulerTest.cpp
@@ -0,0 +1,55 @@
+#include "SchedulerTestBase.h"
+
+
+
+class CallbackSchedulerTest : public SchedulerTestBase {
+protected:
+
+    static int functionCallbackCount;
+    int methodCallbackCount;
+
+    virtual void SetUp() {
+        functionCallbackCount = 0;
+        methodCallbackCount = 0;
+    }
+
+    static void schedulerCallbackFunction() {
+        functionCallbackCount++;
+    }
+
+
+public:
+    void schedulerCallbackMethod() {
+        methodCallbackCount++;
+    }
+
+};
+
+int CallbackSchedulerTest::functionCallbackCount = 0;
+
+
+
+TEST_F(CallbackSchedulerTest, testCallbackSwitchFromFunctionToMethod) {
+    FunctionCallback functionCallback(&CallbackSchedulerTest::schedulerCallbackFunction);
+    MethodCallback<CallbackSchedulerTest> methodCallback(this, &CallbackSchedulerTest::schedulerCallbackMethod);
+
+    CallbackScheduler scheduler;
+
+    scheduler.runPeriodically(500);
+
+    runScheduler(510);
+    ASSERT_EQ(0, functionCallbackCount);
+    ASSERT_EQ(0, methodCallbackCount);
+
+    scheduler.set(&functionCallback);
+    runScheduler(500);
+    ASSERT_EQ(1, functionCallbackCount);
+    ASSERT_EQ(0, methodCallbackCount);
+
+    scheduler.set(&methodCallback);
+    runScheduler(500);
+    ASSERT_EQ(1, functionCallbackCount);
+    ASSERT_EQ(1, methodCallbackCount);
+};
+
+
